46336779918b4ad80c3adab769d29df182d139ce
grapcode <graphcode360@gmail.com>
2025-08-27 10:08:38 +0600

    call added

--- script/index.js
+++ script/index.js
@@ -27,6 +27,15 @@ getId('card-box').addEventListener('click', function (e) {
     const callButton = e.target;
     const cardName = callButton.parentNode.parentNode.children[1].innerText;
     const cardNumber = callButton.parentNode.parentNode.children[3].innerText;
-    console.log(cardName, cardNumber);
+    let coinIcon = parseFloat(getId('coin-icon').innerText);
+    coinIcon = coinIcon - 20;
+    getId('coin-icon').innerText = coinIcon;
+    if (coinIcon > 20) {
+      alert(cardName + ': ' + cardNumber);
+    }
+    if (coinIcon < 20) {
+      alert('❌ আপনার পর্যাপ্ত কয়েন নেই। কল করতে কমপক্ষে ২০ কয়েন লাগবে।');
+    }
+    console.log(cardName, cardNumber, coinIcon);
   }
 });
